c2d95d916016629f7f410587808baaa4db6f6558
isiri <isiri@isiri-desktop.(none)>
2009-03-11 11:28:20 +0530

    latest changes

---
+++ app/controllers/admin/roles_controller.rb
@@ -0,0 +1,13 @@
+class Admin::RolesController < ApplicationController
+  require_role :admin
+  layout 'admin'
+
+  def index
+    @users=User.paginate :page=>params[:page],:per_page=> 10
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @admin_roles }
+    end
+  end
+
+end
