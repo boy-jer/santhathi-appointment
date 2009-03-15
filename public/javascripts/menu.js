6bc44949262f6c3bbaf460ee985486ac60d94f79
vijendra <vijendrakarkala@gmail.com>
2009-03-15 22:49:34 +0530

    style

---
+++ public/javascripts/menu.js
@@ -0,0 +1,51 @@
+/* 
+   Simple JQuery Accordion menu.
+   HTML structure to use:
+
+   <ul id="menu">
+     <li><a href="#">Sub menu heading</a>
+     <ul>
+       <li><a href="http://site.com/">Link</a></li>
+       <li><a href="http://site.com/">Link</a></li>
+       <li><a href="http://site.com/">Link</a></li>
+       ...
+       ...
+     </ul>
+     <li><a href="#">Sub menu heading</a>
+     <ul>
+       <li><a href="http://site.com/">Link</a></li>
+       <li><a href="http://site.com/">Link</a></li>
+       <li><a href="http://site.com/">Link</a></li>
+       ...
+       ...
+     </ul>
+     ...
+     ...
+   </ul>
+
+Copyright 2007 by Marco van Hylckama Vlieg
+
+web: http://www.i-marco.nl/weblog/
+email: marco@i-marco.nl
+
+Free for non-commercial use
+*/
+
+function initMenu() {
+  jQuery('#menu1 ul').hide();
+  jQuery('#menu1 ul:first').show();
+  jQuery('#menu1 li a').click(
+    function() {
+      var checkElement = jQuery(this).next();
+      if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+        return false;
+        }
+      if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+        jQuery('#menu1 ul:visible').slideUp('normal');
+        checkElement.slideDown('normal');
+        return false;
+        }
+      }
+    );
+  }
+jQuery(document).ready(function() {initMenu();});
