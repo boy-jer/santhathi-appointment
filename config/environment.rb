d9f4c01927c591d639639dcb62b91e7e140f6c33
isiri <isiri@isiri-desktop.(none)>
2009-07-15 18:44:38 +0530

    latest changes

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-#RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -16,6 +16,7 @@ Rails::Initializer.run do |config|
   #config.gem "capistrano-ext", :lib => "capistrano"
   config.gem "configatron"
   config.gem "calendar_date_select"
+  #config.gem "ruby-openid", :lib => "openid"
 
   # Make Time.zone default to the specified zone, and make Active Record store time values
   # in the database in UTC, and return them converted to the specified local zone.
