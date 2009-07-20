b0b141c77c404d9152eb2218a08c1503a393c62c
isiri <isiri@isiri-desktop.(none)>
2009-07-20 18:44:36 +0530

    prescribed_test changes

--- app/helpers/laboratory/prescriptions_helper.rb
+++ app/helpers/laboratory/prescriptions_helper.rb
@@ -1,2 +1,6 @@
 module Laboratory::PrescriptionsHelper
+  
+  def lab_report_for_prescribed_test(test)
+     LaboratoryReport.find_by_prescription_id_and_lab_test_id(test.prescription_id, test.lab_test_id )
+  end
 end
