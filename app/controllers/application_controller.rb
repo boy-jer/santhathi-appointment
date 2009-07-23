56ff2e35fe89542e62b8020fc070202438abab7d
Isiri Technologies <isiri@vijendra.(none)>
2009-07-23 20:14:32 +0530

    lab report

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
-# Filters added to this controller apply to all controllers in the application.
+#Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   include RoleRequirementSystem
  # include FaceboxRender
-
+  
+  before_filter :login_require
   helper :all # include all helpers, all the time
   filter_parameter_logging :password, :password_confirmation
+  
 
   # Return the value for a given setting
   def s(identifier)
@@ -29,4 +31,11 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '9fe6825f97cc334d88925fde5c4808a8'
+
+  private
+  def login_require
+    unless logged_in?
+      redirect_to(login_url)
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
 # This controller handles the login/logout function of the site.  
 class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => :create
-  
+  skip_before_filter :login_require, :only => [:new, :create]
+
   layout 'login'
   
   def new
