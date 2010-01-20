75e924e9e57bd3ad18e48014ddca940ce1d0a99e
Isiri <isiri@isiri-desktop.(none)>
2010-01-20 18:12:56 +0530

    modifications to contact groups

--- app/controllers/admin/contact_groups_controller.rb
+++ app/controllers/admin/contact_groups_controller.rb
@@ -27,6 +27,7 @@ class Admin::ContactGroupsController < ApplicationController
   # GET /admin_contact_groups/new.xml
   def new
     @contact_group = ContactGroup.new
+    @contact_lists = ContactList.find(:all)
 
     respond_to do |format|
       format.html # new.html.erb
@@ -37,40 +38,59 @@ class Admin::ContactGroupsController < ApplicationController
   # GET /admin_contact_groups/1/edit
   def edit
     @contact_group = ContactGroup.find(params[:id])
+    @contact_lists = ContactList.find(:all, :conditions => ['contact_group_id = ?', @contact_group.id]) 
+    @non_group_contacts = ContactList.find(:all, :conditions => ['contact_group_id != ?', @contact_group.id]) 
   end
 
   # POST /admin_contact_groups
   # POST /admin_contact_groups.xml
   def create
     @contact_group = ContactGroup.new(params[:contact_group])
-
+    contacts = params[:contacts]
     respond_to do |format|
       if @contact_group.save
-        flash[:notice] = 'Contact group was successfully created.'
-        format.html { redirect_to(admin_contact_groups_url) }
-        format.xml  { render :xml => @contact_group, :status => :created, :location => @contact_group }
+      	unless contacts.blank?
+          contacts.each do  |contact|
+          @contact_group.contact_lists << ContactList.find(contact)
+         end
+      end
+       flash[:notice] = "#{@contact_group.name} was successfully created."
+       format.html { redirect_to(admin_contact_groups_url) }
+       format.xml  { render :xml => @contact_group, :status => :created, :location => @contact_group }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @contact_group.errors, :status => :unprocessable_entity }
       end
     end
+   
   end
 
   # PUT /admin_contact_groups/1
   # PUT /admin_contact_groups/1.xml
   def update
+    
+    
     @contact_group = ContactGroup.find(params[:id])
-
+    contacts = params[:contacts]
+    @contact_group.contact_lists.clear
     respond_to do |format|
-      if @contact_group.update_attributes(params[:contact_group])
-        flash[:notice] = 'Contact group was successfully updated.'
-        format.html { redirect_to(admin_contact_groups_url) }
+      if @contact_group.update_attributes(params[:group])
+      	unless contacts.nil?
+        	contacts.each do  |contact|
+        		 @contact_group.contact_lists << ContactList.find(contact)
+        	end
+        end
+        flash[:notice] = "#{@contact_group.name} was successfully updated."
+         format.html { redirect_to(admin_contact_groups_url) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
         format.xml  { render :xml => @contact_group.errors, :status => :unprocessable_entity }
       end
     end
+
+
+
   end
 
   # DELETE /admin_contact_groups/1
