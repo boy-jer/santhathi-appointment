33822723e49cc041f284a4d2964be0feeea167db
Sanath Kumar <sanath@sanath-laptop.(none)>
2010-02-08 15:51:46 +0530

    bill look and feel

--- app/controllers/pos/bills_controller.rb
+++ app/controllers/pos/bills_controller.rb
@@ -2,6 +2,11 @@ class Pos::BillsController < ApplicationController
   layout 'pms'
 
   def index
-    @bills = Payment.all
+    @search = Payment.new_search(params[:search])
+    @params = params[:search]
+    @search.per_page ||= 25
+    @search.order_as ||= "DESC"
+    @search.order_by ||= "created_at"
+    @bills = @search.all
   end
 end
