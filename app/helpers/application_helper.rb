9e88c788ac2167616012b5feb546983edc6314e6
Sanath Kumar <sanath@sanath-laptop.(none)>
2010-02-15 17:32:43 +0530

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -145,12 +145,14 @@ module ApplicationHelper
                                )
         end
         @prescribed_medicines.each do |ph_med|
-          p.payment_items.build(:payable_name => ph_med.inventory_item.name,
-                                :quantity => ph_med.quantity,
-                                :amount => ph_med.inventory_item.sub_unit_sale_price,
-                                :total_amount => (ph_med.quantity * ph_med.inventory_item.sub_unit_sale_price),
-                                :payable_type => ph_med.inventory_item.class
-                               )
+          unless ph_med.inventory_item.blank?
+            p.payment_items.build(:payable_name => ph_med.inventory_item.name,
+                                  :quantity => ph_med.quantity,
+                                  :amount => ph_med.inventory_item.sub_unit_sale_price,
+                                  :total_amount => (ph_med.quantity * ph_med.inventory_item.sub_unit_sale_price),
+                                  :payable_type => ph_med.inventory_item.class
+                                 )
+          end
         end
       end
       (10 - p.payment_items.length).times do
