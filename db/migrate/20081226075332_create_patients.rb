48f3891181fe6ad9ebe1215920fd8a406818f51a
isiri Technologies <isiri@isiri-desktop.(none)>
2010-06-10 12:30:39 +0530

    db-patient

--- db/migrate/20081226075332_create_patients.rb
+++ db/migrate/20081226075332_create_patients.rb
@@ -12,16 +12,34 @@ class CreatePatients < ActiveRecord::Migration
       t.text :address
       t.string :contact_no
       t.string :reg_type
-
       t.timestamps
     end
 
-   f = File.open("#{RAILS_ROOT}/config/file.csv","r")
+   f = File.open("#{RAILS_ROOT}/config/rashmi.csv","r")
+   i = 0
    f.readlines.each do |record|
      record_split = record.split(",")
-     Patient.create(:reg_date => record_split[0],:reg_no => record_split[1],:patient_name => record_split[2],:dob => record_split[6], :gender=>  record_split[8], :email =>record_split[17], :address => record_split[10], :contact_no => record_split[16])
+     if i >= 1
+       registration_date = record_split[0]
+       split_date = registration_date.split("-")
+       reg_date = split_date[2] + '-' + split_date[0] + '-' + split_date[1]
+       Patient.create(:reg_date => reg_date.to_date,:reg_no => record_split[1],:patient_name => record_split[2],:dob => record_split[6], :gender=>  record_split[8], :email =>record_split[17], :address => record_split[10], :contact_no => record_split[16],:spouse_name => record_split[22])
+      end
+     i = i + 1
     end
-  end
+
+
+    Patient.find(:all).each do |p1|
+      Patient.find(:all).each do |p2|
+        if (p1.patient_name == p2.spouse_name) && (p2.spouse_name == p1.patient_name)
+         p1.update_attributes(:spouse => p2.id)
+         p2.update_attributes(:spouse => p1.id)
+        end
+      end
+    end
+ end
+   
+
 
   def self.down
     drop_table :patients
--- db/migrate/20100125080033_add_field_to_prescribed_test.rb
+++ db/migrate/20100125080033_add_field_to_prescribed_test.rb
@@ -1,9 +1,9 @@
 class AddFieldToPrescribedTest < ActiveRecord::Migration
   def self.up
-    rename_column :prescribed_tests, :lab_test_id, :service_id
-    rename_column :sample_specfications, :lab_test_id, :service_id
-    rename_column :parameter_specifications, :lab_test_id, :service_id
-    rename_column :laboratory_reports, :lab_test_id, :service_id
+    #rename_column :prescribed_tests, :lab_test_id, :service_id
+   # rename_column :sample_specfications, :lab_test_id, :service_id
+    #rename_column :parameter_specifications, :lab_test_id, :service_id
+    #rename_column :laboratory_reports, :lab_test_id, :service_id
     add_column    :services, :duration, :time
   end
 
