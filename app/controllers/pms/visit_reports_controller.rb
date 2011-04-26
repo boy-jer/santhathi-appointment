6fe353db4b5146eb6e0e3ea12f0e3ed2619efad2
user <isiri@ubuntu.ubuntu-domain>
2011-04-26 18:08:36 +0530

    laboratory prescription

--- app/controllers/pms/visit_reports_controller.rb
+++ app/controllers/pms/visit_reports_controller.rb
@@ -21,7 +21,13 @@ class Pms::VisitReportsController < ApplicationController
   end
 
   def new
-    @appointment = Appointment.find(params[:appointment_id])
+   @appointment = Appointment.find(params[:appointment_id])
+   @tests = ['PROLACTIN','IUI','BLOOD GROUP','RBS','FSH','HB','VDRL','TSH','TESTOSTERONE','LH','HIV','HBSAG','ULTRASOUND SCAN']
+   service_ids = []
+   @tests.each  do |test|
+  service_ids << Service.find_by_name(test).id unless Service.find_by_name(test).nil?
+end
+    @prescribed_tests = PrescribedTest.find(:all,:include =>[:prescription],:conditions =>['prescriptions.appointment_id = ? and service_id in (?)' ,@appointment.id,service_ids])
   end
 
    def search
